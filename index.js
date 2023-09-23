9f90d4043f5cac7423da4ac72bde1668e91dd5dd
Guyo <guyo.halake@students.moringaschool.com>
2023-09-23 23:46:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+// const returnFirstTwoDrivers = drivers => drivers.slice(0, 2);
+  };
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+// const returnLastTwoDrivers = drivers => drivers.slice(-2);
+  };
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+// const createFareMultiplier = multiplier => fare => fare * multiplier;
+  };
+  const fareDoubler = createFareMultiplier(2);
+  const fareTripler = createFareMultiplier(3);
+  const selectDifferentDrivers = function(drivers, driverFunction) {
+    return driverFunction(drivers);
+// const selectDifferentDrivers = (drivers, driverFunction) => driverFunction(drivers);
+
+  };
+
+
+
+
+  
